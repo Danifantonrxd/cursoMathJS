4be0239fc45ee32f36c39d98fa1895b334de7bff
Danifanton <danielrxd0912@gmail.com>
2023-02-04 22:00:48 -0400

    Modulo de analisis salarial completado

--- clase_5.js
+++ clase_5.js
@@ -59,3 +59,123 @@ function trabajadoresPorEmpresa(empresa){// [{name: "", nombreEmpresa:[{año:xxx
     }
     return personas;
 }
+
+/*
+{   Salida[Empresa_1][2018][0]
+    Empresa_1: {
+        2018:[Salario, Salario, Salario],
+        2019:[Salario]
+    }
+    Empresa_2:{
+        2018:[Salario, Salario]
+    }
+}
+*/
+
+function salariosPorEmpresa(){
+    let salida = {};
+    for(let i=0; i < salarios.length; i++){
+        for(let j=0; salarios[i].trabajos[j]; j++){
+            let nombreEmpresa = salarios[i].trabajos[j].empresa;
+            let years = salarios[i].trabajos[j].year;
+            let salary = salarios[i].trabajos[j].salario;
+            if(!salida[nombreEmpresa]){
+                salida[nombreEmpresa] = {};
+            }
+            if(!salida[nombreEmpresa][years]){
+                salida[nombreEmpresa][years] = [];
+            }
+            salida[nombreEmpresa][years].push(salary);
+        }
+    }
+    return salida;
+}
+
+function salariosPorEmpresaJuanDC(){
+    let empresas = {};
+    for(persona of salarios){
+        for(trabajo of persona.trabajos){
+            if(!empresas[trabajo.empresa]){
+                empresas[trabajo.empresa] = {};
+            }
+            if(!empresas[trabajo.empresa][trabajo.year]){
+                empresas[trabajo.empresa][trabajo.year] = [];
+            }
+            empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+        }
+    }
+    return empresas;
+}
+
+function medianaEmpresaYear(empresa,year){
+    let aux = salariosPorEmpresa();
+    if(!aux[empresa]){
+        console.log("La empresa no existe");
+        return;
+    }
+    if(!aux[empresa][year]){
+        console.log("La empresa no tuvo trabajadores ese año");
+        return;
+    }
+    return PlatziMath.mediana(aux[empresa][year]);
+}
+
+function proyeccionSalarialEmpresa(empresa){
+    let estructura = salariosPorEmpresa();
+    if(!estructura[empresa]){
+        console.log("La empresa no existe");
+        return;
+    }
+    let medianas = [];
+    let years = Object.keys(estructura[empresa]);
+    for(let i = 0; i < years.length; i++){
+        medianas.push(PlatziMath.mediana(estructura[empresa][years[i]]));
+    }
+    let variaciones = [];
+    for(let i = medianas.length-1; i > 0; i--){
+        let cambio = medianas[i]-medianas[i-1];
+        cambio = cambio / medianas[i-1] * 100;
+        variaciones.push(cambio);
+    }
+    let proyeccion = PlatziMath.promedio(variaciones);
+    proyeccion = medianas[medianas.length-1] + medianas[medianas.length-1]*proyeccion/100;
+    return Number(proyeccion.toFixed(2));
+}
+
+function proyeccionSalarialEmpresaJuanDC(empresa){
+    let estructura = salariosPorEmpresa();
+    if(!estructura[empresa]){
+        console.log("La empresa no existe");
+        return;
+    }
+    let empresaYear = Object.keys(estructura[empresa]);
+    let listaMedianaYears = empresaYear.map((year) => {
+        return medianaEmpresaYear(empresa, year);
+    });    
+    //Hasta aqui fue que me gusto
+
+}
+
+function medianaGeneral(){
+    const medianaPersonas = salarios.map(persona => medianaPorPersona(persona.name));
+    return PlatziMath.mediana(medianaPersonas);
+}
+
+function medianaTop10(){
+    let medianaPersonas = salarios.map(persona => medianaPorPersona(persona.name));
+    medianaPersonas = medianaPersonas.sort((a,b) => a - b);
+    let top10 = Math.round(medianaPersonas.length * 0.1);
+    let j = medianaPersonas.length-1;
+    let aux = [];
+    for(let i = 0; i < top10; i++){
+        aux.push(medianaPersonas[j]);
+        j--;    
+    }
+
+    let limit = medianaPersonas.length - top10;
+    let JuanDC = medianaPersonas.slice(limit, medianaPersonas.length);
+    //El slice COPIA elementos de un arreglo a otro desde una posicion inicial a final
+    //El splice CORTA los elementos de igual manera
+    console.log(JuanDC);
+    return PlatziMath.mediana(aux);
+}
